06416ba702bd3522e9825727141aaed8be5b26dd
YoichiroTacker <youitiro.tacker@gmail.com>
2024-03-17 13:07:12 +0900

    log setings

--- txn.cc
+++ txn.cc
@@ -204,7 +204,7 @@ rc_t transaction::commit() {
     if(is_takada()&& rc._val==RC_INVALID){
       while(rc._val !=RC_TRUE){
         ssn_retry();
-        rc= parallel_ssn_commit();
+        rc = parallel_ssn_commit();
         if (xc->end == 0) {
           return rc_t{RC_ABORT_INTERNAL};
         }
@@ -263,10 +263,10 @@ void transaction::ssn_retry(){
           // remove myself from reader list
           serial_deregister_reader_tx(coro_batch_idx, &r->readers_bitmap);
         }
-        //if (log)
-        //  log->discard();
+        if (log)
+          log->discard();
         
-        //log=nullptr;
+        log=nullptr;
 
         TXN::serial_deregister_tx(coro_batch_idx, xid);
         MM::epoch_exit(xc->end, xc->begin_epoch);
@@ -287,8 +287,9 @@ void transaction::ssn_retry(){
         xc->begin_epoch = config::tls_alloc ? MM::epoch_enter() : 0;
         TXN::serial_register_tx(coro_batch_idx, xid);
         //log = logmgr->new_tx_log((char*)string_allocator().next(sizeof(sm_tx_log_impl))->data());
-        ASSERT(log);
-        xc->begin = logmgr->cur_lsn().offset() + 1;
+        //ASSERT(log);
+        //xc->begin = logmgr->cur_lsn().offset() + 1;
+        xc->begin = rep::GetReadView();
         xc->pstamp = volatile_read(MM::safesnap_lsn);
 
         bool isvalidated=false;
@@ -363,8 +364,9 @@ void transaction::ssn_retry(){
     ASSERT(validated_read_set.empty() and retrying_task_set.empty());
     ALWAYS_ASSERT(state() == TXN::TXN_ACTIVE);
     volatile_write(xc->state, TXN::TXN_COMMITTING);
-    ASSERT(log);
-    xc->end = log->pre_commit().offset();
+    //ASSERT(log);
+    //xc->end = log->pre_commit().offset();
+    xc->end =rep::GetReadView();
     return;
   }
 #endif
